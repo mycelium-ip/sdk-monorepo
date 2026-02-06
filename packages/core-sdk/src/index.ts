f6f8f37f3e2a3f79dfe8d62ddf2c02416712087f
lvl99wzrd <marco.elissa@gmail.com>
2026-02-06 19:47:02 +0700

    refactor: clean up SDK exports and remove unused functions

--- packages/core-sdk/src/index.ts
+++ packages/core-sdk/src/index.ts
@@ -1,6 +1,4 @@
 // This file defines the entire public SDK surface.
 // Do not export internal modules directly.
 
-export function sdkVersion() {
-  return "0.1.0-beta";
-}
+export * from "./transactions";
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,8 +1,2 @@
 // This file defines the entire public SDK surface.
 // Do not export internal modules directly.
-
-import { sdkVersion } from "@mycelium/core-sdk";
-
-export function reactVersion() {
-  return sdkVersion();
-}
