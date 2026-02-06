22d712005aa70ea63156c9b0c3ea51561677f9be
stanlys96 <stanlyskwok@gmail.com>
2026-02-06 19:06:05 +0700

    update programIds

--- packages/core-sdk/src/constants/programIds.ts
+++ packages/core-sdk/src/constants/programIds.ts
@@ -1,15 +1,15 @@
 import { PublicKey } from "@solana/web3.js";
 
 const ENTITY_PROGRAM_ID = new PublicKey(
-  "9wuS9WD7h8J1scE1eUieU9u5mUzMyHAduA9MZcgmxTco",
+  "5n1BMzb89SY53CpMEwg5V6vMMFhWzVPeiyU1ygR114vq",
 );
 
 const IPCORE_PROGRAM_ID = new PublicKey(
-  "GpFaBQ3ihQUVE6711q2fuh6C1qm4EXEQANjRv4thx9PW",
+  "CWrHgr4Kmt1JQ4SPjrETv8dWyrN7qkVMvhYzzJPi5iEh",
 );
 
 const METADATA_PROGRAM_ID = new PublicKey(
-  "Bwa3soySFuwawNvuApEUdrkmdP5ji2ynX3gJn5u6kSJp",
+  "54oAJcxV1w9miM3E3yNrAZzWUCeZrttGp5YkP146w8QB",
 );
 
 export { ENTITY_PROGRAM_ID, IPCORE_PROGRAM_ID, METADATA_PROGRAM_ID };
